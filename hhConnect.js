63f63ff23f977ad994c19378fb12bfe6555948ba
Hugo Rodrigues <hugo@starteffect.com>
2015-01-28 10:04:01 -0800

    Readme++

--- hhConnect.js
+++ hhConnect.js
@@ -1,4 +1,35 @@
-// Helper - Connect to iframe
+/*
+    HackHands Cross-origin iFrame API protocol
+    v2.0.0
+
+    - - - - - - - - - - - - - - - - - - - - - - - - 
+    //Init
+    var HH = hhConnect({ target: <window> })
+
+    - - - - - - - - - - - - - - - - - - - - - - - - 
+
+    // Call a Method
+    HH.call(<functionName>, <params>, <callback>)
+    HH.call('calc', 'hello', function(error, cb){
+
+    });
+
+    // Bind a Method response/logic
+    HH.bind(<functionName>, <callback>)
+    HH.bind('calc', function(params, cb){
+
+    });
+
+    - - - - - - - - - - - - - - - - - - - - - - - - 
+
+    // Listen for an event
+    x.on(<eventName>, <event>)
+
+    // Emit an event
+    x.emit(<eventName>, <params>)
+
+    - - - - - - - - - - - - - - - - - - - - - - - - 
+*/
 var hhConnect = function(userConfig){
 
     // If false or invalid window, return a dummy object (So it wont break apps)
@@ -36,9 +67,8 @@ var hhConnect = function(userConfig){
 
     // Helper: Send the message using an RPC fashion
     var sendMessage = function(name, data, id) {
-        //console.log('sendMessage', [name, data, id])
+        console.log('sendMessage', [name, data, id])
         config.target.postMessage({
-            HH:true,
             namespace: config.namespace, 
             id: id || nextMessageID(), 
             name: name, 
